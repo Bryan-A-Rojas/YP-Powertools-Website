89025c0b2254066fa4af327dced38db107732c0d
Inconsistencies <ketsup22@gmail.com>
2018-03-04 21:21:04 +0800

    Changed the footer, about us, and contact

--- YP Website/pages/aboutus.php
+++ YP Website/pages/aboutus.php
@@ -6,9 +6,18 @@ include '../includes/navbar.inc.php';
 
 ?>
 
-<h1>About us</h1>
+<div class="jumbotron">
+	<div class="container">
+		<h1>About Us</h1>
+		<p>lorem lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque placeat in. Assumenda cupiditate eum, dicta necessitatibus ex inventore similique maiores impedit id sequi, a ut quibusdam, excepturi nostrum ratione.</p>
+	</div>
+</div>
 
-
+<!-- - meaning of yp
+		- mission and vision
+		- year they started
+		- history
+ -->
 <?php 
 
 include '../includes/footer.inc.php';
--- YP Website/pages/contact.php
+++ YP Website/pages/contact.php
@@ -6,8 +6,38 @@ include '../includes/navbar.inc.php';
 
 ?>
 
-<h1>Contact</h1>
 
+<div class="jumbotron">
+    <div class="container">
+    	<div class="row">
+        <h1>Contact</h1>
+        <h2></h2>
+        <p>this company Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque placeat in. Assumenda cupiditate eum, dicta necessitatibus ex inventore similique maiores impedit id sequi, a ut quibusdam, excepturi nostrum ratione.</p>
+        </div>
+        <div class="row">
+        <h2>Locations</h2>
+        <ul>
+            <li>
+                <h3>Marcoz Alvarez, Las Pinas</h3>
+
+            </li>
+        </ul>
+        </div>
+        <h2>Contact Numbers</h2>
+        <ul>
+            <li>0909034043545</li>
+            <li>8090492340924</li>
+            <li>65453445345453</li>
+        </ul>
+        <h2>Email</h2>
+        <p>Loremt@GMAIL.COM</p>
+    </div>
+</div>
+
+	<!-- - google maps of two branches
+		- telephone number
+		- email
+		- phone number -->
 
 <?php 
 
