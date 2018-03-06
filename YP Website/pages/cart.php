a01e9f355a9fa9289a26f1fdf8a633c64d20215e
Inconsistencies <ketsup22@gmail.com>
2018-03-06 20:19:05 +0800

    added stuff to styles again and edited cart, created accountlist_admin

--- YP Website/pages/cart.php
+++ YP Website/pages/cart.php
@@ -7,37 +7,45 @@ include '../includes/navbar_user.inc.php';
 ?>
 
 <div class="container">
-<div class="jumbotron"><h1>Cart</h1></div>
-
+<div class="jumbotron">
+  <h1>Cart</h1>
+<form action="">
+  <input type="Submit" name="clear_cart" value="Clear Cart" class="btn btn-danger btn-lg float-right">
+</form>
+</div>
 
 <table class="table table-hover table-dark">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Product Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Quantity</th>
+      <th scope="col">Description</th>
     </tr>
   </thead>
+  </tfoot>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row"><img src="../images/Available in Stock/Angle Grinder/20180217_145912.jpg" alt="product-img" class="product-image-size"></th>
+      <td>Angle Grinder</td>
+      <td>3000</td>
+      <td>1</td>
+      <td>Wow Such Grinder Much Angle lorem</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <th scope="row"><img src="../images/Available in Stock/Angle Grinder/20180217_145912.jpg" alt="product-img" class="product-image-size"></th>
+      <td>Angle Grinder</td>
+      <td>3000</td>
+      <td>1</td>
+      <td>Wow Such Grinder Much Angle</td>
     </tr>
   </tbody>
+  <tfoot>
+    <tr align="justify" style="color: green; font-size: 25px;">
+      <td>Sum</td>
+      <td>PHP 10,000</td>
+    </tr>
 </table>
 </div>
 
