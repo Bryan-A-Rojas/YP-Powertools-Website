f5b825365f261ea34852b17193994edbf7a37c39
Inconsistencies <ketsup22@gmail.com>
2018-03-06 18:11:21 +0800

    added some stuff in css and added sample pictures for user,admin, superadmin then edited user_page

--- YP Website/pages/user_page.php
+++ YP Website/pages/user_page.php
@@ -8,7 +8,21 @@ include '../includes/navbar_user.inc.php';
 
 ?>
 
-<h1>User</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-md-13" id="profile-transbox">
+			<h2>User's Profile</h2>	
+			<img src="../images/profile_images/sample-user.png" alt="userimage" class="img-circle">
+			<br />
+			<label for="uploading">Change Profile Picture:</label>
+			<br />
+			<input type="file" name="txtuploadpicture" accept="image/*">
+			<br />
+			<label for="email">Your Email:</label>
+			<p>Email should go here</p>
+		</div>
+	</div>
+</div>
 
 <?php 
 
