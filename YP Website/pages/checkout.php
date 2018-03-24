b29d3cbd92b897947e0339ec9a085c559d064c51
Inconsistencies <ketsup22@gmail.com>
2018-03-24 22:13:09 +0800

    added checkout.php

---
+++ YP Website/pages/checkout.php
@@ -0,0 +1,43 @@
+<?php 
+
+require_once '../config.php';
+
+		require_once INCLUDES . 'header.inc.php';
+
+		require_once INCLUDES . 'navbar.inc.php';
+
+?>
+
+<div class="jumbotron" id="jumbotron-color">
+	<div class="container" style="margin-top: 10px; margin-left: 60px">
+		<h2>Checkout</h2>
+	</div>
+</div>
+
+<div class="container" id="form-transbox">
+		<form action="">
+	<div class="form-group row">
+      <label for="" class="col-sm-2 col-form-label">Enter Amount</label>
+       <div class="col-sm-4">
+      <input type="fulladdress" placeholder="Amount" name="txtfulladdress" class="form-control " required>
+  		</div>
+    </div>
+	</form>
+	<div class="row">
+		<div class="col-md-5">Product Name</div>
+		<div class="col-md-5">price</div>
+		<div class="col-md-2">Qty</div>
+	</div>
+	<br />
+	<div class="row">
+		<div class="col-md-6"><a href="products.php">&lt;Back to products page</a></div>
+		<div class="col-md-6"><button type="submit" name="submit" class="btn btn-success float-right">Checkout</button></div>
+	</div>
+	
+</div>
+
+<?php 
+
+		require_once INCLUDES . 'endtags.inc.php';
+
+?>
--- YP Website/pages/signupform.php
+++ YP Website/pages/signupform.php
@@ -22,38 +22,48 @@ require_once INCLUDES . 'header.inc.php';
                 <h1 id="logo-text-sm">Powertools</h1>
                 <div class="container" id="form-transbox">
                 <form action="../includes/scripts/signup.php" method="POST" enctype="multipart/form-data">
-	                <label for="uploading">Upload Profile Picture:</label>
-	                <br>
-	        		<input type="file" name="profile_image" accept="image/*" class="btn btn-primary">
-	        		<br />
-	        		<br />
-	        		<label for="Name">Full Name:</label>
-					<br>
-	        		<input type="text" placeholder="Enter Full Name" name="txtfullname" required>
-	        		<br>
-	        		<label for="email">Email:</label>
-					<br>
-	        		<input type="email" placeholder="Enter Email" name="txtemail" required>
-	        		<br>
-	        		<label for="password">Password:</label>
-	        		<br>
-	        		<input type="password" placeholder="Enter Password" name="txtpassword" required>
-	        		<br>
-	        		<label for="confirmpassword">Confirm Password:</label>
-	        		<br>
-	        		<input type="password" placeholder="Confirm Password" name="txtconfirmpassword" required>
-	        		<br>
-	        		<label for="fulladdress">Enter Full Address:</label>
-	        		<br>
-	        		<input type="fulladdress" placeholder="Enter Full Address" name="txtfulladdress" required>
-					<br>
-	        		<label for="city">City:</label>
-	        		<br>
-	        		<input type="city" placeholder="Enter City" name="txtcity" required>
-	        		<br>
-	        		<br>
-	        		<button type="submit" name="submit" class="btn btn-primary">Sign Up</button>
-	        	</form>
+  <div class="form-group">
+    <label for="uploading">Upload Profile Picture:</label>
+    <input type="file" name="profile_image" accept="image/*" class="form-control-file btn btn-primary" id="exampleFormControlFile1">
+  </div>
+
+  <div class="form-row">
+  <div class="col">
+      <label for="Name">Full Name:</label>
+      <input type="text" placeholder="Enter Full Name" name="txtfullname" class="form-control" required>
+    </div>
+  <div class="col">
+      <label for="email">Email:</label>
+      <input type="email" placeholder="Enter Email" name="txtemail" class="form-control" required>
+    </div>
+  
+    <div class="col">
+      <label for="city">Enter City</label>
+      <input type="city" placeholder="Enter City" name="txtcity" class="form-control" required>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <div class="col">
+      <label for="fulladdress">Enter Full Address</label>
+      <input type="fulladdress" placeholder="Enter Full Address" name="txtfulladdress" class="form-control" required>
+    </div>
+  </div>
+
+  <div class="form-row">
+    <div class="col">
+      <label for="password">Password:</label>
+      <input type="password" placeholder="Enter Password" name="txtpassword" class="form-control" required>
+    </div>
+    <div class="col">
+      <label for="confirmpassword">Confirm Password:</label>
+      <input type="password" placeholder="Confirm Password" name="txtconfirmpassword" class="form-control" required>
+    </div>
+  </div>
+	<br>
+  <button type="submit" name="submit" class="btn btn-primary float-right">Sign Up</button>
+
+</form>
 				<br>
         		<p style="display:inline;">Already have an account? </p><a href="loginform.php" style="display:inline;">Login here</a>
         		</div>
