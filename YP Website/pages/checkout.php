1e7d37f14e4ac55a84454f7fa3f3bd3aa18ea6b6
Inconsistencies <ketsup22@gmail.com>
2018-03-25 20:24:12 +0800

    edited checkout.php

--- YP Website/pages/checkout.php
+++ YP Website/pages/checkout.php
@@ -8,31 +8,41 @@ require_once '../config.php';
 
 ?>
 
+<div style="margin-top: 20px">
+<a href="products.php" style="margin-left: 50px;">&lt;Back to products page</a>
+</div>
 <div class="jumbotron" id="jumbotron-color">
 	<div class="container" style="margin-top: 10px; margin-left: 60px">
-		<h2>Checkout</h2>
-		<a href="products.php">&lt;Back to products page</a>
+		<h1>Checkout</h1>
 	</div>
 </div>
 
 <div class="container" id="form-transbox">
-		<form action="">
-	<div class="form-group row">
-      <label for="" class="col-sm-2 col-form-label">Enter Amount</label>
-       <div class="col-sm-4">
-      <input type="fulladdress" placeholder="Amount" name="txtfulladdress" class="form-control " required>
-  		</div>
-    </div>
-	</form>
-	<div class="row">
-		<div class="col-md-5">Product Name</div>
-		<div class="col-md-5">price</div>
-		<div class="col-md-2">Qty</div>
-	</div>
+
+	<table class="table table-hover table-light">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Price/Piece</th>
+      <th scope="col">Quantity</th>
+    </tr>
+  </thead>
+  <tbody>
+  	<td>Tekken 7</td>
+  	<td>P2500.00</td>
+  	<td>2</td>
 	<br />
-	<div class="row">
-		<div class="col-md-12"><button type="submit" name="submit" class="btn btn-success float-right">Checkout</button></div>
-	</div>
+</tbody>
+</table>
+<div class="row">
+		<div class="col-lg-6">
+		<label for="" class="col-sm-2 col-form-label">Enter Amount</label>
+       	<input type="fulladdress" placeholder="Amount" name="txtfulladdress" class="form-control " required>
+       	</div>
+       	<div class="col-lg-6">
+   			<button type="submit" name="submit" class="btn btn-success float-right">Checkout</button>
+       	</div>	
+</div>
 	
 </div>
 
