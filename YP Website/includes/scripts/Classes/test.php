ad14aeb5a116ea8eba5af798d9aa01e5d15e94d2
Judge 47 <johnyrocketfingers12@gmail.com>
2018-04-01 01:55:47 +0800

    Added something in TODO

--- YP Website/includes/scripts/Classes/test.php
+++ YP Website/includes/scripts/Classes/test.php
@@ -2,37 +2,73 @@
 	
 	//Basic pagination with no error checking
 
-	require_once '../../../config.php';
-	require_once SCRIPTS . 'dbh.inc.php';
+	 require_once '../../../config.php';
 
-	//Number of items per page
-	$limit = 2;
+	 require_once INCLUDES . 'header.inc.php';
+	// require_once SCRIPTS . 'dbh.inc.php';
 
-	//First found out the number of items in the database
-	$sql = "SELECT COUNT(product_id)
-			FROM products;";
+	// //Number of items per page
+	// $limit = 2;
 
-	$result = $Database->query($sql);
+	// //First found out the number of items in the database
+	// $sql = "SELECT COUNT(product_id)
+	// 		FROM products;";
 
-	$num_of_products = $result->fetch_assoc();
-	$num_of_products = $num_of_products[0];
+	// $result = $Database->query($sql);
 
-	echo $num_of_products;
+	// $num_of_products = $result->fetch_assoc();
+	// $num_of_products = $num_of_products[0];
 
-	//Query String
-	$sql = "";
+	// echo $num_of_products;
 
-	//if()
-	$sql = "SELECT *
-			FROM products
-			LIMIT 0, $limit";
+	// //Query String
+	// $sql = "";
 
-	$result = $Database->query($sql);
+	// //if()
+	// $sql = "SELECT *
+	// 		FROM products
+	// 		LIMIT 0, $limit";
 
-	while($row = $result->fetch_assoc()){
-		echo "<pre>";
-		print_r($row);
-		echo "</pre>";
-	}
+	// $result = $Database->query($sql);
+
+	// while($row = $result->fetch_assoc()){
+	// 	echo "<pre>";
+	// 	print_r($row);
+	// 	echo "</pre>";
+	// }
 
 ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6">
+			<canvas id="myChart" style="width:500px;height:500px"></canvas>
+		</div>
+	</div>	
+</div>
+
+<script>
+	var ctx = document.getElementById('myChart').getContext('2d');
+	
+	var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+</script>
