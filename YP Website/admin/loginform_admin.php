490721a1f90dfdf137cc0368914e7ba862583c66
Inconsistencies <ketsup22@gmail.com>
2018-04-02 10:24:42 +0800

    added design to loginform_admin

--- YP Website/admin/loginform_admin.php
+++ YP Website/admin/loginform_admin.php
@@ -6,6 +6,8 @@ require_once INCLUDES . 'header.inc.php';
 
 ?>
 
+<div style="background-image: url(../images/powertooloop.gif); background-repeat: no-repeat; background-size: 1400px;">
+
     <div class="container">
         <div class="row">
         	<!-- Dividers to center logo -->
@@ -14,7 +16,8 @@ require_once INCLUDES . 'header.inc.php';
             	<!-- Logo image and text -->
                 <img src="../images/icons/yplogoblk.png" id="logo-padding-sm">
                 <h1 id="logo-text-sm">Powertools</h1>
-                <div class="container" id="form-transbox" style="margin-bottom: 100px; margin-left: 50px; margin-right: 200px">
+                <div class="col-lg-12" style="margin-left: 80px">
+                <div class="container" id="form-transbox" style="border-radius: 15px; margin-bottom: 100px; margin-left: 50px; margin-right: 200px">
                 <form action="scripts/login.php" method="POST">
 	                <div class="form-group">
 					    <label for="email">Email:</label>
@@ -31,15 +34,20 @@ require_once INCLUDES . 'header.inc.php';
 					  </div>
 					<br />
 
-					<button type="submit" name="submit" class="btn btn-success float-right">Log In</button>
+					<button type="submit" name="submit" class="btn btn-success" style="margin-left: 160px">Log In</button>
 				</form>
+				</div>
 				</div>
             </div>
 	    </div>
     </div>
+
+    
 
 <?php 
 
 require_once INCLUDES . 'endtags.inc.php';
 
 ?>
+
+</div>
